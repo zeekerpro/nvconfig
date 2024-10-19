571172d748765e3c1bbabe630be126a2bc6e9643
zeeker <zeekerpro@gmail.com>
2024-10-18 23:12:48 -0700

    isolate opts and deps of avante.vim

--- lua/custom/plugins/installs/avante.lua
+++ lua/custom/plugins/installs/avante.lua
@@ -1,7 +1,7 @@
 -- https://github.com/yetone/avante.nvim
 
 
-local M = {
+local opts = {
   ---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
   provider = "openai", -- Recommend using Claude
   auto_suggestions_provider = "openai", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
@@ -73,7 +73,7 @@ local M = {
       -- Open the 'AvanteAsk' prompt in a floating window
       floating = false,
       -- Start insert mode when opening the ask window, only effective if floating = true.
-      start_insert = false,
+      start_insert = true,
       border = "rounded",
     },
   },
@@ -92,4 +92,42 @@ local M = {
   },
 }
 
-return M;
+local deps = {
+  "nvim-treesitter/nvim-treesitter",
+  "stevearc/dressing.nvim",
+  "nvim-lua/plenary.nvim",
+  "MunifTanjim/nui.nvim",
+  --- The below dependencies are optional,
+  "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+  "zbirenbaum/copilot.lua", -- for providers='copilot'
+  {
+    -- support for image pasting
+    "HakonHarnes/img-clip.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- recommended settings
+      default = {
+        embed_image_as_base64 = false,
+        prompt_for_file_name = false,
+        drag_and_drop = {
+          insert_mode = true,
+        },
+        -- required for Windows users
+        use_absolute_path = true,
+      },
+    },
+  },
+  {
+    -- Make sure to set this up properly if you have lazy=true
+    'MeanderingProgrammer/render-markdown.nvim',
+    opts = {
+      file_types = { "markdown", "Avante" },
+    },
+    ft = { "markdown", "Avante" },
+  },
+}
+
+return {
+  opts = opts,
+  deps = deps
+};
