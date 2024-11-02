895616c69e387d41e6873c390f28834ba5bcf729
zeeker <zeekerpro@gmail.com>
2024-11-02 09:39:02 -0700

    lazygit

--- lua/custom/plugins/installs/init.lua
+++ lua/custom/plugins/installs/init.lua
@@ -174,6 +174,28 @@ return {
     build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
   },
 
+  -- nvim v0.8.0
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    }
+  },
+
   -- ai completions
   {
     'zbirenbaum/copilot.lua',
